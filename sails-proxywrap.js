3facd4390fbfb4f80fc260c70d2e8a4bed7d207e
Matthew Fowle <matthewf@webs.com>
2016-05-24 13:18:40 -0400

    Properly pass proxywraphttp{,s} values into Proxyquire. Fixed now!

--- sails-proxywrap.js
+++ sails-proxywrap.js
@@ -1,7 +1,7 @@
 "use strict"
-var proxyquire = require("proxyquire"), proxywraphttp = require("./proxywraphttp")
+var proxyquire = require("proxyquire"), proxywraphttp = require("./proxywraphttp"), proxywraphttps = require("./proxywraphttps")
 
 module.exports = proxyquire("sails",  {
-	"http": "proxywraphttp",
-	"https": "proxywraphttps"
+	"http": proxywraphttp,
+	"https": proxywraphttps
 })
